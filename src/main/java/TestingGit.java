b640eee8bef3f307acc4af71dff16c83b0c3cd71
Haziq Rahat <47474227+haziqrahat@users.noreply.github.com>
2023-06-26 16:54:32 -0700

    Update TestingGit.java

--- src/main/java/TestingGit.java
+++ src/main/java/TestingGit.java
@@ -2,6 +2,7 @@ public class TestingGit {
 
     public static void main(String[] args) {
         System.out.println("  branch");
+        System.out.println(" good branch");
         System.out.println("Hello");
     }
 }
